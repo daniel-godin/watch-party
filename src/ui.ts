972df67cb67db238fb43f6969241c63e7e6b47c5
Daniel Godin <DanielEGodin@gmail.com>
2024-04-19 10:19:12 -0700

    cleanup

--- src/ui.ts
+++ src/ui.ts
@@ -3,9 +3,9 @@ import { randomIdGenerator } from "./utils";
 
 // Firebase Imports:
 import { auth, db,  } from "./firebase";
-import { setDoc, doc, getDoc, getDocs, onSnapshot, updateDoc, arrayUnion, arrayRemove, query, where, collection, deleteDoc } from "firebase/firestore";
+import { setDoc, doc, onSnapshot, updateDoc, collection, deleteDoc } from "firebase/firestore";
 
-const pageContainer = document.getElementById('pageContainer');
+const pageContainer = document.getElementById('pageContainer'); // This is on every html page.  Maybe change to use body later?
 
 function buildUI() {
     console.log('buildUI function triggered');
@@ -14,7 +14,7 @@ function buildUI() {
     createFooterUI();
 }
 
-buildUI();
+buildUI(); // Triggers the UI build.
 
 function createNavUI() {
     pageContainer?.insertAdjacentHTML('beforebegin', `
@@ -27,25 +27,10 @@ function createNavUI() {
 }
 
 function createMainUI() {
-    // if statements to decide which html to load.  if url is... index.html, demo.html, watch-party.html.
-    const pathname = window.location.pathname;
-    // console.log(pathname);
-
-
-    if (pathname == '/index.html' ||  pathname == '/' || pathname.length === 0) {
-        // console.log('createMainUI index Page Activated');
-        createIndexPageUI();
-    }
-    if (pathname == '/demo.html') {
-        // console.log('createMainUI Demo Page Activated');
-        createDemoPageUI();
-    }
-    if (pathname == '/watch.html') {
-        // console.log('createMainUI Watch Party Page Activated');
-        createWatchPartyUI();
-    }
-
-
+    const pathname = window.location.pathname; // Finding pathname to sort which UI function to trigger.
+    if (pathname == '/index.html' ||  pathname == '/' || pathname.length === 0) { createIndexPageUI(); };  
+    if (pathname == '/demo.html') { createDemoPageUI(); };
+    if (pathname == '/watch.html') { createWatchPartyUI(); };
 }
 
 function createFooterUI() {
@@ -57,7 +42,7 @@ function createFooterUI() {
 }
 
 async function createIndexPageUI() {
-    console.log('createIndexPageUI triggered');
+
     pageContainer?.insertAdjacentHTML('afterbegin', `
         <div id='newWatchPartyContainer' class='main-container'>
             <h1>Create Your Watch Party (save the link/unique watch party id)</h1>
@@ -72,12 +57,10 @@ async function createIndexPageUI() {
 
     const newWatchPartyForm = document.getElementById('newWatchPartyForm');
     const inputTitle = document.getElementById('inputTitle');
-    const btnCreateWatchParty = document.getElementById('btnCreateWatchParty');
-
 
     newWatchPartyForm?.addEventListener('submit', async (e) => {
         e.preventDefault();
-          // console.log('newWatchPartyForm submitted');
+
         const watchPartyID = randomIdGenerator();
     
         const watchPartyObj = {
@@ -109,30 +92,23 @@ async function createIndexPageUI() {
             console.log("Title Doc created with ID: ", titleID);
         } catch (e) {
             console.error("Error adding document: ", e);
-        }
-          
+        }  
     })
 }
 
 async function createDemoPageUI() {
-
     // This should look through the 00-demoWatchParty document in the Firestore database 
     // and create an editable UI.
 
+    // Later... Creates a shallow copy to play around with on a local level.  Does not alter main firebase doc, this way others can play with it too.
+
     const docRef = doc(db, "watchParties", "00-demoWatchParty");
-    // const docSnap = await getDoc(docRef);
-
-
 
     onSnapshot(docRef, (snapshot) => {
 
         pageContainer.innerHTML = ''; // This resets the pageContainer DOM so it doesn't duplicate when it's updated.
         
-        // console.log("onSnapshot function triggered: ", snapshot);
         if (snapshot.exists()) {
-            // Create the Demo Page UI.
-            // console.log("Document data:", docSnap.data());
-    
             const data = snapshot.data();
     
             const watchPartyID = data.watchPartyID;
@@ -151,10 +127,7 @@ async function createDemoPageUI() {
             `)
     
         } else {
-            // Demo Page UI not working.  Make UI that says "error, not working, go back to index page."
-            // docSnap.data() will be undefined in this case
-            console.log("No such document!");
-    
+            // snapshot does not exist.  Creates an Error on the DOM and link to go back to home page.
             pageContainer?.insertAdjacentHTML('afterbegin', `
                 <p>Error:  Document not loading.  Please go back to <a href='./index.html'>Main Page</a>.</p>
             `)
@@ -162,14 +135,13 @@ async function createDemoPageUI() {
     });
 
     const colRef = collection(db, 'watchParties', '00-demoWatchParty', 'titleOptions');
-
     onSnapshot(colRef, (snapshot) => {
 
         const watchPartyForm = document.getElementById('watchPartyForm');
 
-        watchPartyForm.innerHTML = '';
+        watchPartyForm.innerHTML = ''; // Resets DOM every time onSnapshot is triggered.  Prevents duplicates.
 
-        let count = 0;
+        let count = 0; // Count to keep titles to 10 and below.  If statements below.
 
         snapshot.forEach((document) => {
             count++;
@@ -195,56 +167,50 @@ async function createDemoPageUI() {
                     </div>
                 </div>
             `)
-            
-            console.log(document.id, " =>", document.data());
         })
 
         const arrayOfBtnRemoveTitle = document.getElementsByClassName('button-remove-title');
-
         for (let i = 0; i < arrayOfBtnRemoveTitle.length && i < 11; i++) {
             arrayOfBtnRemoveTitle[i].addEventListener('click', async (e) => {
-                console.log('Minus Clicked: ', e.target.dataset.id);
-
                 await deleteDoc(doc(db, "watchParties", "00-demoWatchParty", 'titleOptions', e.target.dataset.id));
             });
         }
 
         if (count < 10) {
             watchPartyForm?.insertAdjacentHTML('beforeend', `
-                    <div id='addTitleContainer'>
-                        <input type='text' id='addTitleInput' placeholder='Add Another Title Here' />
-                        <button type='button' id='btnAddTitle'>+</button>
-                    </div>
-                `)
+                <div id='addTitleContainer'>
+                    <input type='text' id='addTitleInput' placeholder='Add Another Title Here' />
+                    <button type='button' id='btnAddTitle'>+</button>
+                </div>
+            `)
     
-                const addTitleInput = document.getElementById('addTitleInput');
-                const btnAddTitle = document.getElementById('btnAddTitle');
+            const addTitleInput = document.getElementById('addTitleInput');
+            const btnAddTitle = document.getElementById('btnAddTitle');
     
-                btnAddTitle?.addEventListener('click', async (e) => {
-                    e.preventDefault();
-                    console.log('add title button clicked');
+            btnAddTitle?.addEventListener('click', async (e) => {
+                e.preventDefault();
 
-                    const id = randomIdGenerator();
-    
-                    const newTitleObject = {
-                        id: id,
-                        title: addTitleInput.value,
-                        links: {
-                            tmdb: "https://link.com",
-                        },
-                        votes: {
-                            yes: 0,
-                            maybe: 0,
-                            no: 0
-                        }
-                    }
+                const id = randomIdGenerator();
 
-                    try {
-                        setDoc(doc(db, 'watchParties', '00-demoWatchParty', 'titleOptions', id), newTitleObject);
-                    } catch (e) {
-                        console.error("Eerror adding document: ", e);
+                const newTitleObject = {
+                    id: id,
+                    title: addTitleInput.value,
+                    links: {
+                        tmdb: "https://link.com",
+                    },
+                    votes: {
+                        yes: 0,
+                        maybe: 0,
+                        no: 0
                     }
-                })
+                }
+
+                try {
+                    setDoc(doc(db, 'watchParties', '00-demoWatchParty', 'titleOptions', id), newTitleObject);
+                } catch (e) {
+                    console.error("Eerror adding document: ", e);
+                }
+            })
         }
 
         if (count >= 10) { console.log('Only 10 options allowed at this time.  Please remove a title if you wish to add a different one.')}
@@ -288,8 +254,6 @@ async function createWatchPartyUI() {
             const btnCopyWatchPartyID = document.getElementById('btnCopyWatchPartyID');
             const btnCopyWatchPartyURL = document.getElementById('btnCopyWatchPartyURL');
 
-            console.log('copyTest', copyWatchPartyIDText, copyWatchPartyURLText);
-
             btnCopyWatchPartyID.addEventListener('click', () => {
                 navigator.clipboard.writeText(copyWatchPartyIDText);
                 console.log('Copied Unique Watch Party ID');
@@ -299,10 +263,6 @@ async function createWatchPartyUI() {
                 navigator.clipboard.writeText(copyWatchPartyURLText);
                 console.log('Copied Unique Watch Party URL');
             })
-
-
-
-
 
         } else {
             // Watch Page Didn't Find the document.  Load a quick page with a search form, a header, and a way to go back to index.
@@ -321,8 +281,10 @@ async function createWatchPartyUI() {
 
     onSnapshot(colRef, (snapshot) => {
         const watchPartyForm = document.getElementById('watchPartyForm');
+
         watchPartyForm.innerHTML = '';
-        let count = 0;
+
+        let count = 0; // Count to keep titles to 10 or less.  Check if(statements) below.
 
         snapshot.forEach((document) => {
             count++;
@@ -354,8 +316,6 @@ async function createWatchPartyUI() {
 
         for (let i = 0; i < arrayOfBtnRemoveTitle.length && i < 11; i++) {
             arrayOfBtnRemoveTitle[i].addEventListener('click', async (e) => {
-                console.log('Minus Clicked: ', e.target.dataset.id);
-
                 await deleteDoc(doc(db, "watchParties", watchPartyIDFromURL, 'titleOptions', e.target.dataset.id));
             });
         };
@@ -373,7 +333,6 @@ async function createWatchPartyUI() {
     
                 btnAddTitle?.addEventListener('click', async (e) => {
                     e.preventDefault();
-                    console.log('add title button clicked');
 
                     const id = randomIdGenerator();
     
@@ -401,7 +360,4 @@ async function createWatchPartyUI() {
         if (count >= 10) { console.log('Only 10 options allowed at this time.  Please remove a title if you wish to add a different one.')}
 
     })
-
-    const testPartyID = '1713534667375-0e7ce2c4-ca14-4d06-b551-d506bc12e104';
-
 }
