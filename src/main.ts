72bc9bd56c711e9ae4c948ea156697abfcd095b6
Daniel Godin <DanielEGodin@gmail.com>
2024-04-20 16:01:32 -0700

    Forgot to import to main.ts"

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import './fav-96.png';
 import './ui.ts';
 import './data.ts';
 import './tmdbUtilities.ts';
+import './random-TV-episode.ts';
 import './watch-party-demo.ts';
+import './watch-party.ts';
+
 
 // Firebase Imports:
 import { app, db, auth } from "./firebase.ts";
